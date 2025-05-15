97c53061ec567bded372d704e95e8facadfa5614
Alwx <alwxsin@gmail.com>
2025-05-16 00:09:51 +0300

    lsp config setup end

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -35,7 +35,7 @@ return {
 						mode = mode or "n"
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
-                    
+
 					map("P", vim.lsp.buf.hover, "Show hover window with declaration")
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
@@ -74,15 +74,20 @@ return {
 					--  the definition of its *type*, not where it was *defined*.
 					map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
 
+					map("[d", function()
+						vim.diagnostic.jump({ count = 1 })
+					end, "Go to next problem")
+					map("]d", function()
+						vim.diagnostic.jump({ count = -1 })
+					end, "Go to prev problem")
+
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if
-						client
-					then
+					if client then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -110,9 +115,7 @@ return {
 					-- code, if the language server you are using supports them
 					--
 					-- This may be unwanted, since they displace some of your code
-					if
-						client
-					then
+					if client then
 						map("<leader>th", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 						end, "[T]oggle Inlay [H]ints")
@@ -148,24 +151,6 @@ return {
 					end,
 				},
 			})
-			-- local autocmd = vim.api.nvim_create_autocmd
-
-			-- autocmd("LspAttach", {
-			-- 	callback = function(e)
-			-- 		local opts = { buffer = e.buf }
-			--
-			-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			-- 		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-			-- 		vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-			-- 		vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-			-- 		vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-			-- 		vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-			-- 		vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-			-- 		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-					vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-			-- 	end,
-			-- })
 		end,
 	},
 	{

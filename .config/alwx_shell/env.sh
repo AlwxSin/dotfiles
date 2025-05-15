ca0412619e27e58c8759476737e35a9524380394
Alwx <alwxsin@gmail.com>
2025-05-15 19:40:27 +0300

    add an option to use local env files (with secrets e.g.)

--- .config/alwx_shell/env.sh
+++ .config/alwx_shell/env.sh
@@ -1,3 +1,8 @@
 source "$(dirname "$0")/aliases.sh"
 source "$(dirname "$0")/exports.sh"
 source "$(dirname "$0")/functions.sh"
+
+local_env="$(dirname "$0")/local.sh"
+if [ -f $local_env ]; then
+    source $local_env
+fi

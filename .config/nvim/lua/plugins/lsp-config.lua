360af18ab77edb6a7c668437f23b0ec3051dea0e
Alwx <alwxsin@gmail.com>
2025-05-30 15:50:27 +0300

    add snacks plugin

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -153,6 +153,8 @@ return {
         opts = {
             library = {
                 { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                { path = "snacks.nvim", words = { "Snacks" } },
+                { path = "lazy.nvim", words = { "LazyVim" } },
             },
         },
     },

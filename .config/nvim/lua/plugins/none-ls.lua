9598885c4b4c24fb394e9086a9c585b4fde71445
alwx <alwxsin@gmail.com>
2025-08-07 18:38:57 +0300

    tweak lsp

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,18 +1,24 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.golangci_lint,
-				null_ls.builtins.formatting.gofumpt,
-				-- null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.golines,
-				-- null_ls.builtins.formatting.gci,
-			},
-		})
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                -- null_ls.builtins.diagnostics.golangci_lint.with({
+                --     method = null_ls.methods.DIAGNOSTICS_ON_DEMAND,
+                -- }),
+                null_ls.builtins.formatting.gofumpt,
+                -- null_ls.builtins.formatting.goimports_reviser,
+                null_ls.builtins.formatting.golines,
+                -- null_ls.builtins.formatting.gci,
+            },
+        })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "lsp format buffer" })
-	end,
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "lsp format buffer" })
+        -- vim.keymap.set("n", "<leader>gl", function()
+        --     local bufnr = vim.api.nvim_get_current_buf()
+        --     null_ls.diagnostics.run({}, bufnr)
+        -- end, { desc = "Run lint manually" })
+    end,
 }
